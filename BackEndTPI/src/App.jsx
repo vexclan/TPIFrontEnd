6f21e5d19b25046e82f516fc1b971f8a69d672e3
vexclan <diegolautaro60mez@gmail.com>
2024-10-29 19:10:06 -0300

    estructura básica de redirección

--- BackEndTPI/src/App.jsx
+++ BackEndTPI/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Component } from 'react'
+import { Route, Switch, Redirect } from 'wouter'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default class app extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      menu:"login"
+    }
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  render(){
+    return(
+      <>
+      <Switch>
+        <Route path="/"><Redirect to="/login"/></Route>
+        <Route path="/login">
+          <FormularioInicio/>
+        </Route>
+        <Route path="/">
+          <FormularioInicio/>
+        </Route>
+        <Route>404</Route>
+      </Switch>
+      </>
+        
+
+    )
+  }
 }
-
-export default App
---
+++ BackEndTPI/src/componentes/Boton.jsx
@@ -0,0 +1,17 @@
+import { Component } from 'react'
+
+export default class Boton extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <span 
+            className='Boton'
+            onClick={()=> this.props.click()}
+            >{this.props.titulo}</span>
+        )
+    }
+}
+
